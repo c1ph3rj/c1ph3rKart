ee148e406e4ad9956b6d9a2bb5633f4bf6d0fc29
c1ph3rj <jeevarag1016@gmail.com>
2022-10-20 06:46:59 +0530

    morning commit on 20/10/2022 06:47AM

--- app/src/main/java/com/c1ph3r/c1ph3rkart/SelectedItem.java
+++ app/src/main/java/com/c1ph3r/c1ph3rkart/SelectedItem.java
@@ -5,6 +5,7 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.c1ph3r.c1ph3rkart.Adapter.ProductImagesAdapter;
 import com.c1ph3r.c1ph3rkart.Model.ProductList;
@@ -14,17 +15,26 @@ import java.util.Objects;
 public class SelectedItem extends AppCompatActivity {
     ProductList selectedItem;
     String value = "";
+    TextView productName, brandName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selected_item);
         Intent intent = getIntent();
+        productName = findViewById(R.id.selectedProductName);
+        brandName = findViewById(R.id.selectedProductBrandName);
         selectedItem = (ProductList) intent.getSerializableExtra("selectedProduct");
         value = intent.getStringExtra("value");
 
         ViewPager productImages = findViewById(R.id.productImages);
         ProductImagesAdapter imageAdapter = new ProductImagesAdapter(this, selectedItem.getImages());
         productImages.setAdapter(imageAdapter);
+
+        productName.setText(selectedItem.getTitle());
+        brandName.setText(selectedItem.getBrand());
+
+
     }
     public void onBackPressed() {
         Intent intent = new Intent(this,ProductsInTheData.class);
