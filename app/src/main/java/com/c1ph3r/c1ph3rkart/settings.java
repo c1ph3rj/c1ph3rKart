5992e94820b2eeb7096099a54ace6b3ca4e21500
c1ph3rj <jeevarag1016@gmail.com>
2022-10-29 07:20:40 +0530

    Commit on 29/10/2022 07:20AM

--- app/src/main/java/com/c1ph3r/c1ph3rkart/settings.java
+++ app/src/main/java/com/c1ph3r/c1ph3rkart/settings.java
@@ -11,13 +11,19 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import com.c1ph3r.c1ph3rkart.DBHealper.UserDataBaseHelper;
+import com.c1ph3r.c1ph3rkart.Model.userDetail;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class settings extends Fragment {
     View view;
-    TextView userDetails, orderHistory, logout;
+    TextView  orderHistory, logout, userName, userEmail;
+    ConstraintLayout userDetailsLayout;
+    MaterialButton loginToContinue;
 
     public settings() {
     }
@@ -33,24 +39,40 @@ public class settings extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_settings, container, false);
         if (view != null) {
-            userDetails = view.findViewById(R.id.userDetails);
             orderHistory = view.findViewById(R.id.orderHistory);
             logout = view.findViewById(R.id.logout);
+            userName = view.findViewById(R.id.userUserName);
+            userEmail = view.findViewById(R.id.userUserEMail);
+            userDetailsLayout = view.findViewById(R.id.layoutForUserDetails);
+            loginToContinue = view.findViewById(R.id.loginToContinue);
             SharedPreferences sharedPreferences = requireActivity().getSharedPreferences("userId", MODE_PRIVATE);
             String user = sharedPreferences.getString("userName", "");
+            UserDataBaseHelper userDB = new UserDataBaseHelper(requireActivity());
+            userDetail userData = userDB.getUserData(user);
 
-            if (user.equals(""))
+            if (user.equals("")) {
                 logout.setVisibility(View.GONE);
+                userDetailsLayout.setVisibility(View.GONE);
+                loginToContinue.setVisibility(View.VISIBLE);
+            }else{
+                logout.setVisibility(View.VISIBLE);
+                userDetailsLayout.setVisibility(View.VISIBLE);
+                loginToContinue.setVisibility(View.GONE);
+                userName.setText(userData.getUserName());
+                userEmail.setText(userData.getEMail());
+            }
 
-            userDetails.setOnClickListener(userStatusView -> {
-                String userName = sharedPreferences.getString("userName", "");
-                if (userName.equals("")) {
-                    Toast.makeText(requireActivity(), "Login to Continue!", Toast.LENGTH_SHORT).show();
+
+
+            loginToContinue.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
                     Intent intent = new Intent(requireActivity(), LoginScreen.class);
                     startActivity(intent);
-                } else
-                    Toast.makeText(requireActivity(), "hello " + userName, Toast.LENGTH_SHORT).show();
+                }
             });
+
+
 
             logout.setOnClickListener(userLogoutView -> {
                 MaterialAlertDialogBuilder alertDialogBuilder = new MaterialAlertDialogBuilder(requireActivity());
